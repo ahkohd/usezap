c2e17baed85a82c4263b681ae2af58f3f4e2c428
Victor Aremu <victor.olorunbunmi@gmail.com>
2023-01-21 23:31:08 +0100

    chore: update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ahkohd', // Usually your GitHub org/user name.
-  projectName: 'switch-desktop', // Usually your repo name.
+  projectName: 'usezap', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -111,7 +111,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/ahkohd/usezap',
               },
             ],
           },
