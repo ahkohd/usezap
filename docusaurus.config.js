7ec4748e5da7c059f755e94d2fd8d7631eeb37c7
Victor Aremu <victor.olorunbunmi@gmail.com>
2023-01-29 13:15:20 +0100

    feat: write featues list

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,53 +1,54 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Zap / Manual',
-  tagline: "A Vim inspired Dock for  macOS",
-  url: 'https://usezap.sh',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: "Zap",
+  tagline:
+    "Maximize your productivity on macOS with a Dock designed for efficiency, inspired by the popular text editor Vim.",
+  url: "https://usezap.sh",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ahkohd', // Usually your GitHub org/user name.
-  projectName: 'usezap', // Usually your repo name.
+  organizationName: "ahkohd", // Usually your GitHub org/user name.
+  projectName: "usezap", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -57,61 +58,61 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Zap',
+        title: "Zap",
         logo: {
-          alt: 'Zap logo',
-          src: 'img/logo.png',
+          alt: "Zap logo",
+          src: "img/logo.png",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Tutorial",
           },
-          { to: '/blog', label: 'Blog', position: 'left' },
+          { to: "/blog", label: "Blog", position: "left" },
           {
-            href: 'https://github.com/ahkohd/usezap',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/ahkohd/usezap",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: "Tutorial",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: "Discord",
+                href: "https://discord.gg/zxpkReVe",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/usezap',
+                label: "Twitter",
+                href: "https://twitter.com/usezap",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Blog",
+                to: "/blog",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/ahkohd/usezap',
+                label: "GitHub",
+                href: "https://github.com/ahkohd/usezap",
               },
             ],
           },
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,43 +1,100 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Navigate at the Speed-of-Thought",
+    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Bind your favorite apps with custom shortcuts and switch to them with a
+        single keystroke. Focus on what truly matters and take your productivity
+        to new heights.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "A Dock-as-a-Spotlight",
+    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Arrange your dock as needed and access all your apps in a single,
+        organized window for quick and convenient access.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Effortless Context Switching",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Experience seamless productivity with app spaces, where different work
+        contexts are elevated to a whole new level. Effortlessly create,
+        organize, and switch between these spaces to maximize your workflow.
+      </>
+    ),
+  },
+  {
+    title: "Seamless Multi-screen support",
+    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+    description: (
+      <>
+        Maximize your multi-screen setup with Zap's seamless support. Quickly
+        switch to apps on other screens and have your mouse automatically moved
+        to the window for effortless navigation.
+      </>
+    ),
+  },
+  {
+    title: "Vim-inspired features",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+    description: (
+      <>
+        Bring the power of Vim to your app management with Zap's Vim-inspired
+        mark feature. Quickly mark and recall app windows with ease, freeing up
+        your mind to focus on your work.
+      </>
+    ),
+  },
+  {
+    title: "Vim-inspired Navigation",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+    description: (
+      <>
+        Elevate your productivity with Zap's Vim-inspired navigation.
+        Effortlessly switch between docked apps using a simple and intuitive
+        interface, modeled after the popular text editor Vim.{" "}
+      </>
+    ),
+  },
+  {
+    title: "Your Cmd+Tab Replacement?",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+    description: (
+      <>
+        Say goodbye to slow and cluttered app switching with Zap's Cmd+Tab
+        replacement feature. Enable this in the preferences and enjoy
+        lightning-fast, efficient app switching.
+      </>
+    ),
+  },
+  {
+    title: "Optimized for Speed and Productivity",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+    description: (
+      <>
+        Zap is designed for speed, efficiency, and lightweight performance.
+        Built using Rust, every action is optimized for quick access and
+        seamless productivity.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
